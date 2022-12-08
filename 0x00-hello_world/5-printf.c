07cb906da3cb734a21e2196613efa0b9d976f265
liza-emo <elizabethomae@gmail.com>
2022-12-08 10:19:43 -0800

    Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints arbitrary string to the console
+ *
+ * Return: 0 if code is successful, -1 if errors
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
