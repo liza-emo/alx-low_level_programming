8a9a0b66c6f0138972924b9fb1a06357f254886b
liza-emo <elizabethomae@gmail.com>
2022-12-08 10:04:08 -0800

    Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints arbitrary string to the console
+ *
+ * Return: 0 if code if successful, -1 for errors
+ */
+int main(void) 
+{  
+char my_string[] = "\"Programming is like building a multilingual puzzle";  
+puts(my_string);
+return (0);  
+}
