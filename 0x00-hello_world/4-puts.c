adcfda50a593764a958c8d77870682cf314a79ff
liza-emo <elizabethomae@gmail.com>
2022-12-08 10:07:08 -0800

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,9 @@
  *
  * Return: 0 if code if successful, -1 for errors
  */
-int main(void) 
-{  
-char my_string[] = "\"Programming is like building a multilingual puzzle";  
+int main(void)
+{
+char my_string[] = "\"Programming is like building a multilingual puzzle";
 puts(my_string);
-return (0);  
+return (0);
 }
