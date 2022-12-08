fd34f14aa36dd71dc98fdda35faf1c9b6cdaff7d
liza-emo <elizabethomae@gmail.com>
2022-12-08 10:22:24 -0800

    Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - prints sizes of C data types to the console
+
+ *
+
+ * Return: 0 if code is succesful, -1 if errors
+
+ */
+int main(void) 
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));  
+printf("Size of a long int: %d byte(s)\n", sizeof(long));  
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));  
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
