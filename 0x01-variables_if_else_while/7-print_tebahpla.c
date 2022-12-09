bd176484a7013d3e6eabb1bcbb3f1512f18c14c9
liza-emo <elizabethomae@gmail.com>
2022-12-09 14:18:41 -0800

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -Entry point
+ *
+ *Description: lowercase alphabet in reverse
+ *
+ * Return: 0 if successful, -1 if errors
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; --c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
