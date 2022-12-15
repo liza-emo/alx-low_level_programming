ff37370dcf817778ab7310d7ff4b4b64c4d882e6
liza-emo <elizabethomae@gmail.com>
2022-12-14 17:13:48 -0800

    Nested loop and functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *_islower - checks for lowercase character
+  *
+  *Return: 1 if lowercase, 0 otherwise
+  */
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
