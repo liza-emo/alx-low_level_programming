b1dd6d68e77f9b2f12da9c8900cfe7f96056a41e
liza-emo <elizabethomae@gmail.com>
2022-12-14 18:16:41 -0800

    Nested loop and functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - Entry point
+ *@n is an integer
+ *Description: prints the absolute vaue of an integer
+ *Return: integer
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
