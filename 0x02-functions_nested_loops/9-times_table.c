2377cfe9f3c7cfdb59d6d3534c1b3ce7acdc12ca
liza-emo <elizabethomae@gmail.com>
2022-12-14 18:53:34 -0800

    Nested loop and functions

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiplication tables
+ *
+ * Return: null
+ */
+void times_table(void)
+{
+	int counter1;
+	int counter2;
+	int result;
+	for (counter1 = 0; counter1 < 10; counter1++)
+	{
+		for (counter2 = 0; counter2 < 10; counter2++)
+		{
+			result = counter2 * counter1;
+			if (result < 10)
+			{
+				if (counter2 > 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar ((result % 10) + '0');
+			}
+			if (counter2 != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	return;
+}
