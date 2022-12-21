0b9430c23dcebee3ca5ebb56f893ade7b312cb4c
liza-emo <elizabethomae@gmail.com>
2022-12-20 18:31:49 -0800

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int checker = 0;
-while (*(src + checker) != '\0')
-{
-*(dest + checker) = *(src + checker);
-checker++;
-}
-*(dest + (checker + 1)) = '\0';
-return (dest);
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
 }
