ce83ddfabe418f9188c8995b712c2fd6f10a1849
liza-emo <elizabethomae@gmail.com>
2022-12-20 18:28:33 -0800

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,13 @@
  */
 void print_array(int *a, int n)
 {
-int counter;
-int array_length = sizeof(*a) / sizeof(int);
-if (n != array_length)
-{
-n = array_length;
-}
-for (counter = 0; counter < n - 1; counter++)
-{
-printf("%d, ", *(a + counter));
-}
-printf("%d\n", *(a + (n - 1)));
-return;
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
 }
