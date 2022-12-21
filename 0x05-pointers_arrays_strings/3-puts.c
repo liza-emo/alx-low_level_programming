d451832ee829500f57188657971f3246c71bd60e
liza-emo <elizabethomae@gmail.com>
2022-12-20 17:29:09 -0800

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_puts - prints string followed by new line
+ *@str: input string
+ *Return: No return value
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
