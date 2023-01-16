d39b95f8eb64145d2d1f8e44bf4257bf0c2e940a
Elizabeth Omae <104896287+liza-emo@users.noreply.github.com>
2023-01-16 22:07:19 +0300

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - generates keygen.
- * Return: 0 Always.
- */
+* main - Print a password
+*
+* Return: Always 0.
+*/
+
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+int myrand;
+int count;
+int total;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+srand(time(NULL));
+for (count = 0, total = 2772; total > 122; count++)
+{
+myrand = (rand() % 125) + 1;
+printf("%c", myrand);
+total -= myrand;
+}
+printf("%c", total);
+
+return (0);
 }
