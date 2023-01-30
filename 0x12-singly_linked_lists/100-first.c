16d3d048d2373dad6e272395490aac3df8750328
liza-emo <104896287+liza-emo@users.noreply.github.com>
2023-01-31 00:04:24 +0300

    Advanced Tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * bmain - function executed before main
+ * Return: no return.
+ */
+
+void constructor_func(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
