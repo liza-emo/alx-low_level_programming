2ecacab376fc8a7b5c050017ee63cffd7f8b3f74
liza-emo <104896287+liza-emo@users.noreply.github.com>
2023-01-30 22:54:52 +0300

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to head of list to be printed
+ * Return: number of nodes in linked list
+*/
+
+size_t print_list(const list_t *h)
+{
+    int i = 0;
+
+    if (h == NULL)
+    {
+        printf("[0] (nil)\n");
+    }
+    while (h != NULL)
+    {
+        printf("[%u] %s\n", h ->len, h->str);
+        h = h -> next;
+        i++;
+    }
+    return (i);
+}
