f62b1f49a7b95dc3e90305e1c5c5d44fe44b78ca
liza-emo <104896287+liza-emo@users.noreply.github.com>
2023-01-30 23:15:18 +0300

    Length of list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,15 +8,12 @@
 
 size_t list_len(const list_t *h)
 {
-    int i = 0;
+    size_t i = 0;
 
-    if (h == NULL)
-    {
-        return (0);
-    }
     while (h != NULL)
     {
         i++;
+        h = h->next;
     }
     return (i);
 }
